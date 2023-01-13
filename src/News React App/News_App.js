19008189f8ceaebf1f9ac2b79360c465a4f16f1c
Samshad Ali <118520464+Samshad-Ali@users.noreply.github.com>
2023-01-14 00:37:51 +0530

    Update News_App.js

--- src/News React App/News_App.js
+++ src/News React App/News_App.js
@@ -1,25 +1,28 @@
 import React, { useEffect, useRef, useState } from "react";
+import Loading from "./Loading";
 import News_Card from "./News_Card";
 import "./News_Card.css";
 
 const News_App = () => {
   const [newsList, setNewsList] = useState([]);
+  const [page, setPage] = useState(1);
   const [query, setQuery] = useState("tesla");
-  const [info, setInfo] = useState("India");
-  const queryDataref = useRef(null);
- const [input,setInput]=useState('')
-  const apiKey = "ef5b66227eac4ea68f6b04522c4ca1d6";
-  const apiURL = `  https://newsapi.org/v2/everything?q=${query}&from=2022-12-12&sortBy=publishedAt&apiKey=ef5b66227eac4ea68f6b04522c4ca1d6
+  const [loading, setLoding] = useState(false);
+  const [input, setInput] = useState("");
+  const apiKey = "2af1db7184314a2e87176a0a0c6e37be";
+  const apiURL = ` https://newsapi.org/v2/everything?q=${query}&from=2022-12-13&sortBy=publishedAt&apiKey=2af1db7184314a2e87176a0a0c6e37be&page=${page}&pageSize=9
   `;
 
   useEffect(() => {
     fetchData();
-  }, [query]);
+  }, [query,page]);
 
   async function fetchData() {
     try {
+      setLoding(true);
       const response = await fetch(apiURL);
       const Data = await response.json();
+      setLoding(false);
       setNewsList(Data.articles);
     } catch (error) {
       console.log("error occured", error);
@@ -29,12 +32,16 @@ const News_App = () => {
   function handleSubmit(e) {
     e.preventDefault();
     setQuery(input);
-    setInput('')
+    setInput("");
   }
 
-  function handleBtn(e) {
-    setQuery(info);
-  }
+  const handlePreBtn = () => {
+    setPage(page-1)
+  };
+  const handleNexBtn = () => {
+
+    setPage(page+1);
+  };
 
   return (
     <div className="container">
@@ -43,7 +50,9 @@ const News_App = () => {
         <div className="search-box">
           <input
             type="text"
-            onChange={(e) => { setInput(e.target.value) }}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
             value={input}
             placeholder="Search the news here"
             className="search"
@@ -58,7 +67,13 @@ const News_App = () => {
       <div className="trending-topics">
         <h2>Trending Topics</h2>
         <div className="particular-btn">
-          <button onClick={handleBtn}>India</button>
+          <button
+            onClick={() => {
+              setQuery("India");
+            }}
+          >
+            India
+          </button>
           <button
             onClick={() => {
               setQuery("Share Market");
@@ -91,13 +106,23 @@ const News_App = () => {
       </div>
 
       <div className="box">
-        {newsList.map((news, id) => {
-          return <News_Card data={news} key={id} />;
-        })}
+        {loading ? (
+          <div className="loading">
+            {" "}
+            <Loading />{" "}
+          </div>
+        ) : (
+          newsList.map((news, id) => {
+            return <News_Card data={news} key={id} />;
+          })
+        )}
+      </div>
+      <div className="last-btn">
+        <button disabled={page<=1?true:false} onClick={handlePreBtn}> <i className="fa-solid fa-caret-left"></i>  Prev</button>
+        <button onClick={handleNexBtn}>Next <i className="fa-solid fa-caret-right"></i></button>
       </div>
     </div>
   );
 };
 
 export default News_App;
-
